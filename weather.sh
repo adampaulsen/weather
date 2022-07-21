fed0708c9b48022c8e115666771f1d9df24a70d7
Adam <adam.paulsen@gmail.com>
2022-07-21 08:49:14 -0700

    Update weather.sh
    updated lat4 and lon4 variables to use 'bc' instead of 'printf' to format the trailing number digits behind the decimal.

--- weather.sh
+++ weather.sh
@@ -13,8 +13,8 @@ fi
 url=$'https://api.geoapify.com/v1/geocode/search?text='$1'&lang=en&limit=10&type=postcode&filter=countrycode:us&apiKey='$APIKEY''
 lat=$(curl -s "$url" | jq -r '.features[].properties.lat')
 lon=$(curl -s "$url" | jq -r '.features[].properties.lon')
-lat4=$(printf "%8.3f" "$lat")
-lon4=$(printf "%8.3f" "$lon")
+lat4=$(bc <<< "scale=4;$lat/1")
+lon4=$(bc <<< "scale=4;$lon/1")
 latlon=$(echo $lat4 | xargs),$(echo $lon4 | xargs)
 point=$(curl -s "https://api.weather.gov/points/$latlon" | jq -r '.properties["forecast"]')
 curl -s "${point}" | jq -r '.properties.periods[] | "\(.name):\(.temperature):\(.shortForecast)"' | awk -F':' '{ printf "%-16s  %d  %-60s\n", $1, $2, $3 }'
